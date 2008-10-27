1188467a4e49b787c46d0ca114690058c44a1b97
Will Larson <lethain@gmail.com>
2008-10-27 09:41:24 -0400

    Added support for rendering django-compress groups instead of filename.

--- userskins/templatetags/userskins.py
+++ userskins/templatetags/userskins.py
@@ -1,5 +1,9 @@
 
 from django import template
+try:
+    from compress.templatetags import compressed
+except ImportError:
+    compressed = None
 
 register = template.Library()
 
@@ -13,6 +17,6 @@ class UserskinNode(template.Node):
         skin = template.Variable("userskins_skin").resolve(context)
         use_compress = template.Variable("userskins_use_compress").resolve(context)
         if use_compress:
-            pass
+            return compress.compressed_css(skin)
         else:
             return u'<link rel="stylesheet" href="%s">' % skin

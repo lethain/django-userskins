398f0c3b381797dbe65297ca06688efface652fc
Will Larson <lethain@gmail.com>
2008-10-27 09:11:04 -0400

    Reworked context so that non-authenticated can have skins as well (based on cookies).

--- dev_userskins/urls.py
+++ dev_userskins/urls.py
@@ -1,11 +1,25 @@
 from django.conf.urls.defaults import *
+from django.template import RequestContext
+from django.http import HttpResponseRedirect
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
 
+def dark(request):
+    hrr = HttpResponseRedirect("/")
+    hrr.set_cookie("userskins", "dark")
+    return hrr
+
+def light(request):
+    hrr = HttpResponseRedirect("/")
+    hrr.set_cookie("userskins", "light")
+    return hrr
+
 urlpatterns = patterns(
     '',
     (r'^$','django.views.generic.simple.direct_to_template',
      {'template':'dev_index.html'}),
+    (r'^dark/$',dark),
+    (r'^light/$',light),
 )
--- userskins/context.py
+++ userskins/context.py
@@ -4,15 +4,14 @@ from django.conf import settings
 
 def userskins(request):
     skin = settings.USERSKINS_DEFAULT
-    if request.user.is_authenticated():
-        if request.COOKIES.has_key("userskins"):
-            skin = request.COOKIES["userskins"]
-        else:
-            try:
-                skin = SkinPreference.objects.get(user=request.user).skin
-            except SkinPreference.ObjectDoesNotExist:
-                pass
-            request.COOKIES["userskins"] = skin
+    if request.COOKIES.has_key("userskins"):
+        skin = request.COOKIES["userskins"]
+    elif request.user.is_authenticated():
+        try:
+            skin = SkinPreference.objects.get(user=request.user).skin
+        except SkinPreference.ObjectDoesNotExist:
+            pass
+        request.COOKIES["userskins"] = skin
     if settings.USERSKINS_USE_COMPRESS_GROUPS:
         return {"userskins_skin": skin, "userskins_use_compress":True }
     else:

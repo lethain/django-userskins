9af8f1bb9a98ac57878d36185cf68cccb200935d
Will Larson <lethain@gmail.com>
2008-10-27 09:35:33 -0400

    Updated urls to include the django dev static server.

--- dev_userskins/urls.py
+++ dev_userskins/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls.defaults import *
 from django.template import RequestContext
 from django.http import HttpResponseRedirect
+from django.conf import settings
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
@@ -18,6 +19,7 @@ def light(request):
 
 urlpatterns = patterns(
     '',
+    (r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT }),
     (r'^$','django.views.generic.simple.direct_to_template',
      {'template':'dev_index.html'}),
     (r'^dark/$',dark),

5d1b60a3fe8f2c2564e94fd646f4e112f5803288
Will Larson <lethain@gmail.com>
2008-10-29 21:38:11 -0400

    Fixed typo of ObjectDoesNotExist versus DoesNotExist for error thrown when user doesn't have a SkinPreference object.

--- userskins/middleware.py
+++ userskins/middleware.py
@@ -2,20 +2,23 @@ from django.conf import settings
 from userskins.models import SkinPreference
 import django
 
+
 class UserskinsMiddleware(object):
     def __init__(self):
         never_use_database = getattr(settings,"USERSKINS_NEVER_ACCESS_DATABASE", False)
         if never_use_database:
             raise django.core.exceptions.MiddlewareNotUsed
+        else:
+            self.default = settings.USERSKINS_DEFAULT
+
 
     def process_response(self, request, response):
-        print "wee"
         if not request.COOKIES.has_key("userskins"):
-            skin = settings.USERSKINS_DEFAULT
+            skin = self.default
             if request.user.is_authenticated():
                 try:
                     skin = SkinPreference.objects.get(user=request.user).skin
-                except SkinPreference.ObjectDoesNotExist:
+                except SkinPreference.DoesNotExist:
                     pass
             response.set_cookie("userskins", skin)
         return response

a955b23d19806828c41e03759cccd6d18bb64e4f
Will Larson <lethain@gmail.com>
2009-02-20 14:46:04 -0500

    Removed print statements.

--- userskins/templatetags/userskins.py
+++ userskins/templatetags/userskins.py
@@ -15,11 +15,9 @@ def userskin(parser, token):
 class UserskinNode(template.Node):
     def render(self, context):
         skin = template.Variable("userskins_skin").resolve(context)
-        print skin
         use_compress = template.Variable("userskins_use_compress").resolve(context)
         if use_compress:
             node = compressed.CompressedCSSNode(skin)
-            print node
             return node.render({skin:skin})
             
         else:
